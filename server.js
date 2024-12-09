350f5b00c435aef8740ccc7903607031c7c927d4
Patrick Ruff <patruff@gmail.com>
2024-12-08 19:38:05 -0500

    Add Express.js server for audio files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'));
+
+// Serve audio files
+app.use('/audio', express.static('audio'));
+
+// Serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
